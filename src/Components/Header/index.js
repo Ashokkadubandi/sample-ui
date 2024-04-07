49a4dc5e26b5952d5e8e88befff46f13a8e3d402
Ashok <velmaashok765@gmail.com>
2024-04-07 13:48:28 +0000

    Add Responsiveness

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,24 +1,47 @@
+import { useState } from "react";
 import "./index.css";
 
-const Header = () => (
-  <>
-    <div className="header">
-      <h1>ACO</h1>
-      <ul className="mid-text">
+const Header = () => {
+  const [menu, setDyn] = useState(false);
+
+  const changeMenu = () => {
+    setDyn(!menu);
+  };
+
+  const getDataLists = () => {
+    const ActiveBar = menu ? "mobile-links-view" : "";
+    return (
+      <ul className={`mobile-links ${ActiveBar}`}>
         <li>Home</li>
         <li>About</li>
         <li>Contact</li>
         <li>Support</li>
+        <li>Sign in</li>
       </ul>
-      <button type="button">Signin</button>
-    </div>
-    <div className="mobile-view">
-      <h1>ACO</h1>
-      <button type="button" className="mobile-btn">
-        CLick
-      </button>
-    </div>
-  </>
-);
+    );
+  };
+
+  return (
+    <>
+      <div className="header">
+        <h1>ACO</h1>
+        <ul className="mid-text">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Support</li>
+        </ul>
+        <button type="button">Signin</button>
+      </div>
+      <div className="mobile-view">
+        <h1>ACO</h1>
+        <button type="button" className="mobile-btn" onClick={changeMenu}>
+          {menu ? "X" : "CLICK"}
+        </button>
+        {getDataLists()}
+      </div>
+    </>
+  );
+};
 
 export default Header;
