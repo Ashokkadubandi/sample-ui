600b51492136725034bbe9abc21977af4df2355e
Ashok <velmaashok765@gmail.com>
2024-04-07 13:17:41 +0000

    Solution

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,16 +1,24 @@
 import "./index.css";
 
 const Header = () => (
-  <div className="header">
-    <h1>ACO</h1>
-    <ul className="mid-text">
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-      <li>Support</li>
-    </ul>
-    <button type="button">Signin</button>
-  </div>
+  <>
+    <div className="header">
+      <h1>ACO</h1>
+      <ul className="mid-text">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Support</li>
+      </ul>
+      <button type="button">Signin</button>
+    </div>
+    <div className="mobile-view">
+      <h1>ACO</h1>
+      <button type="button" className="mobile-btn">
+        CLick
+      </button>
+    </div>
+  </>
 );
 
 export default Header;
